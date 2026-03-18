bf14f2dabcb5ee647ac71b7b723e2a5e59b77b62
3DR impressões <3drimpressoes@gmail.com>
2026-03-18 15:37:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3909534254-106ad",
+  "appId": "1:170890003497:web:d0573d67ce4ebf1fb60838",
+  "apiKey": "AIzaSyDOjfQxHEensTPfk74o0zVBVJnWb389CVs",
+  "authDomain": "studio-3909534254-106ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "170890003497"
+};
